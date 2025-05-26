82a7432e6e365e23bb0f1005c20f76c3fcce1d2f
phims <149851683+phims403@users.noreply.github.com>
2025-05-26 22:38:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # config.py
-BOT_TOKEN = "masukkan token bot tele disini"
+BOT_TOKEN = "masukkan bot token disini"
 CHAT_ID = "masukkan chat id disini"
-
-# cara membuat bot  telegram  dan mengambil token dan chat id tonton ini https://drive.google.com/file/d/1Kcy_tZXyWV4TxLk5Vq4pfyyiRkyKHxGo/view?usp=drivesdk
+# jangan buang tanda petiknya (")
+# contoh "abcd1234"
+# cara membuat bot  telegram  dan mengambil token dan chat id
+tonton ini
+https://drive.google.com/file/d/1Kcy_tZXyWV4TxLk5Vq4pfyyiRkyKHxGo/view?usp=drivesdk
