aa1defd3db46c282b435948d2c44a9944a4f927f
phims <149851683+phims403@users.noreply.github.com>
2025-07-11 21:33:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,14 @@
 # config.py
-
 # Bor telegram
-
 BOT_TOKEN = "bot token"
-
 CHAT_ID = "chat id"
-
 # Email Configuration (untuk fitur kirim laporan via email)
-
 EMAIL_PENGIRIM = "email akun gmail"          # <- Ganti dengan email Gmail kamu
-
 EMAIL_PASSWORD = "password tanpa spasi"             # <- Gunakan App Password Gmail
-
 NAMA_PELAPOR = "nama"                      # <- Nama pelapor untuk laporan otomatis
-
 # jangan buang tanda petiknya (")
-
 # contoh "abcd1234"
-
 # cara membuat bot  telegram  dan mengambil token dan chat id tonton ini
-
 # https://drive.google.com/file/d/12J-PEJcvJuv7PpX1DXBWQOCIQFcMIyeu/view?usp=drivesdk
-
 # cara setting gmail tonton ini
-
 # https://drive.google.com/file/d/12F5cYBm8b5KVKkKmsa_1Yenfrqvcv5IG/view?usp=drivesdk
-
-
